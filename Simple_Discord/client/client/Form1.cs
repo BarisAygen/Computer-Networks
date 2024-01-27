53b7b0a0d43be933f9f17050442458a90458292f
Barış Aygen <99341549+BarisAygen@users.noreply.github.com>
2024-01-27 12:54:11 +0300

    Add files via upload

---
+++ Simple_Discord/client/client/Form1.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace client {
+    public partial class Form1 : Form {
+        bool terminating = false; // Initially it is not terminating
+        bool isUsernameValid = true; // Initially assume the username is valid
+        bool connected = false; // Initially it is not connected
+        Socket clientSocket;
+        public Form1() // Start the form
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            this.FormClosing += new FormClosingEventHandler(Form1_FormClosing);
+            InitializeComponent();
+        }
+        private void button_connect_Click(object sender, EventArgs e) // When client clicks to connect
+        {
+            string username = textBox_username.Text;
+            if (string.IsNullOrWhiteSpace(username)) // Check if username is empty or consists only of white-space
+            {
+                logs_connect.AppendText("Username cannot be empty!\n"); // Let user know
+                return; // Prevent further execution if username is empty
+            }
+            clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            string IP = textBox_ip.Text;
+            int portNum;
+            if (Int32.TryParse(textBox_port.Text, out portNum))
+            {
+                try
+                {
+                    clientSocket.Connect(IP, portNum);
+                    // Edit buttons properties accordingly
+                    button_connect.Enabled = false;
+                    button_disconnect.Enabled = true;
+                    logs_connect.Enabled = true;
+                    button_subscribe_if100.Enabled = true;
+                    button_subscribe_sps101.Enabled = true;
+                    connected = true; // Update connection status
+                    Byte[] buffer = Encoding.Default.GetBytes(username);
+                    clientSocket.Send(buffer);
+                    Thread receiveThread = new Thread(Receive);
+                    receiveThread.Start(); // Create and start the thread
+                    Thread.Sleep(500); // Give some time for the server to respond
+                    if (!isUsernameValid)
+                    {
+                        // If username is not valid, reset the connection
+                        clientSocket.Close();
+                        connected = false; // Update the connection status
+                        isUsernameValid = true; // Reset for next attempt
+                        // Edit buttons properities accordingly
+                        button_connect.Enabled = true;
+                        button_disconnect.Enabled = false;
+                        button_subscribe_if100.Enabled = false;
+                        button_subscribe_sps101.Enabled = false;
+                        return;
+                    }
+                }
+                catch // Wrong IP
+                {
+                    logs_connect.AppendText("Could not connect to the server!\n");
+                }
+            }
+            else // Wrong port number
+            {
+                logs_connect.AppendText("Check the port\n");
+            }
+
+        }
+
+        private void Receive()
+        {
+            while (connected)
+            {
+                try
+                {
+                    Byte[] buffer = new Byte[64];
+                    clientSocket.Receive(buffer);
+                    string incomingMessage = Encoding.Default.GetString(buffer);
+                    incomingMessage = incomingMessage.Substring(0, incomingMessage.IndexOf("\0"));
+
+                    if (incomingMessage == "Username exists. Please choose another username.")
+                    {
+                        logs_connect.AppendText(incomingMessage + "\n");
+                        isUsernameValid = false; // Set the flag to false if username is not valid
+                        break; // Break out of the loop as the server will close the connection
+                    }
+                    else if (incomingMessage.StartsWith("Connected"))
+                    {
+                        logs_connect.AppendText(incomingMessage + "\n");
+                        isUsernameValid = true; // Set the flag to true
+                        connected = true;
+                        button_connect.Enabled = false;
+                        button_disconnect.Enabled = true;
+                        button_subscribe_if100.Enabled = true;
+                        button_subscribe_sps101.Enabled = true;
+                    }
+                    if (incomingMessage.StartsWith("subscribed:"))
+                    {
+                        string channel = incomingMessage.Split(':')[1];
+                        if (channel == "if100")
+                        {
+                            // Edit form properities accordingly
+                            logs_if100.Enabled = true;
+                            textBox_message_if100.Enabled = true;
+                            button_send_if100.Enabled = true;
+                            button_subscribe_if100.Enabled = false;
+                            button_unsubscribe_if100.Enabled = true;
+                            logs_connect.AppendText("Subscribed to the IF100 channel.\n");
+                        }
+                        else if (channel == "sps101")
+                        {
+                            // Edit form properities accordingly
+                            logs_sps101.Enabled = true;
+                            textBox_message_sps101.Enabled = true;
+                            button_send_sps101.Enabled = true;
+                            button_subscribe_sps101.Enabled = false;
+                            button_unsubscribe_sps101.Enabled = true;
+                            logs_connect.AppendText("Subscribed to the SPS101 channel.\n");
+                        }
+                    }
+                    else if (incomingMessage.StartsWith("if100:"))
+                    {
+                        string channelMessage = incomingMessage.Substring(11); // Remove the "if100:" prefix
+                        logs_if100.AppendText(channelMessage + "\n");
+                    }
+                    else if (incomingMessage.StartsWith("sps101:"))
+                    {
+                        string channelMessage = incomingMessage.Substring(12); // Remove the "sps101:" prefix
+                        logs_sps101.AppendText(channelMessage + "\n");
+                    }
+
+                }
+                catch
+                {
+                    if (!terminating)
+                    {
+                        logs_connect.AppendText("The server has disconnected\n");
+                        button_connect.Enabled = true;
+                        textBox_message_if100.Enabled = false;
+                        button_send_if100.Enabled = false;
+                        textBox_message_sps101.Enabled = false;
+                        button_send_sps101.Enabled = false;
+                    }
+
+                    clientSocket.Close();
+                    connected = false;
+                }
+            }
+            if (!isUsernameValid)
+            {
+                // Reset UI and connection status if username is not valid
+                Invoke(new Action(() =>
+                {
+                    button_connect.Enabled = true;
+                    button_disconnect.Enabled = false;
+                    button_subscribe_if100.Enabled = false;
+                    button_subscribe_sps101.Enabled = false;
+                }));
+            }
+
+
+        }
+
+        private void Form1_FormClosing(object sender, System.ComponentModel.CancelEventArgs e) // CLose the form
+        {
+            connected = false; // Update connection status
+            terminating = true; // Termination
+            Environment.Exit(0);
+        }
+
+        private void button_send_if100_Click(object sender, EventArgs e)
+        {
+            string message = textBox_message_if100.Text;
+            if (message != "" && message.Length <= 64)
+            {
+                string formattedMessage = "if100:" + message; // Format the message to indicate it's for the if100 channel
+                Byte[] buffer = Encoding.Default.GetBytes(formattedMessage);
+                clientSocket.Send(buffer);
+            }
+        }
+        private void button_disconnect_Click(object sender, EventArgs e)
+        {
+            if (connected)
+            {
+                string disconnectMessage = "disconnect"; // Send a message to the server indicating that the client is disconnecting
+                Byte[] buffer = Encoding.Default.GetBytes(disconnectMessage);
+                clientSocket.Send(buffer);
+
+                clientSocket.Close(); // Close the socket and update the connection status
+                connected = false;
+                terminating = true;
+
+                // Edit form properities
+                button_connect.Enabled = true;
+                button_disconnect.Enabled = false;
+                button_subscribe_if100.Enabled = false;
+                button_subscribe_sps101.Enabled = false;
+                button_unsubscribe_if100.Enabled = false;
+                button_unsubscribe_sps101.Enabled = false;
+                textBox_message_if100.Enabled = false;
+                textBox_message_sps101.Enabled = false;
+                button_send_if100.Enabled = false;
+                button_send_sps101.Enabled = false;
+                logs_if100.Enabled = false;
+                logs_sps101.Enabled = false;
+                logs_connect.AppendText("Disconnected from the server.\n");
+            }
+            else
+            {
+                logs_connect.AppendText("You are not connected to a server.\n");
+            }
+        }
+
+        private void button_subscribe_if100_Click(object sender, EventArgs e)
+        {
+            if (connected)
+            {
+                string subscribeMessage = "subscribe:if100"; // This is the message you will send to the server
+                Byte[] buffer = Encoding.Default.GetBytes(subscribeMessage);
+                clientSocket.Send(buffer); // Send message to the server
+                logs_connect.AppendText("Attempting to subscribe to if100...\n");
+            }
+            else
+            {
+                logs_connect.AppendText("You are not connected to a server.\n");
+            }
+        }
+
+        private void button_unsubscribe_if100_Click(object sender, EventArgs e)
+        {
+            if (connected)
+            {
+                string unsubscribeMessage = "unsubscribe:if100"; // Unsubscribe request format
+                Byte[] buffer = Encoding.Default.GetBytes(unsubscribeMessage);
+                clientSocket.Send(buffer);
+
+                // Update properities accordingly
+                button_unsubscribe_if100.Enabled = false;
+                textBox_message_if100.Enabled = false;
+                button_send_if100.Enabled = false;
+                button_subscribe_if100.Enabled = true;
+
+                logs_connect.AppendText("Unsubscribed from if100 channel.\n");
+            }
+            else
+            {
+                logs_connect.AppendText("You are not connected to a server.\n");
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_subscribe_sps101_Click(object sender, EventArgs e)
+        {
+            if (connected)
+            {
+                string subscribeMessage = "subscribe:sps101"; // This is the message you will send to the server
+                Byte[] buffer = Encoding.Default.GetBytes(subscribeMessage);
+                clientSocket.Send(buffer); // Send message to the server
+                logs_connect.AppendText("Attempting to subscribe to sps101...\n");
+            }
+            else
+            {
+                logs_connect.AppendText("You are not connected to a server.\n");
+            }
+        }
+
+        private void button_unsubscribe_sps101_Click(object sender, EventArgs e)
+        {
+            if (connected)
+            {
+                string unsubscribeMessage = "unsubscribe:sps101"; // Unsubscribe request format
+                Byte[] buffer = Encoding.Default.GetBytes(unsubscribeMessage);
+                clientSocket.Send(buffer);
+
+                // Update properities accordingly
+                button_unsubscribe_sps101.Enabled = false;
+                textBox_message_sps101.Enabled = false;
+                button_send_sps101.Enabled = false;
+                button_subscribe_sps101.Enabled = true;
+
+                logs_connect.AppendText("Unsubscribed from sps101 channel.\n");
+            }
+            else
+            {
+                logs_connect.AppendText("You are not connected to a server.\n");
+            }
+        }
+
+        private void button_send_sps101_Click(object sender, EventArgs e)
+        {
+            string message = textBox_message_sps101.Text;
+
+            if (message != "" && message.Length <= 64)
+            {
+                string formattedMessage = "sps101:" + message; // Format the message to indicate it's for the sps101 channel
+                Byte[] buffer = Encoding.Default.GetBytes(formattedMessage);
+                clientSocket.Send(buffer);
+            }
+        }
+    }
+}
---
+++ Simple_Discord/server/server/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_port = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button_listen = new System.Windows.Forms.Button();
+            this.logs = new System.Windows.Forms.RichTextBox();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.textBox_message = new System.Windows.Forms.TextBox();
+            this.button_send = new System.Windows.Forms.Button();
+            this.logs_users = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.logs_if100_subscriptions = new System.Windows.Forms.RichTextBox();
+            this.logs_sps101_subscriptions = new System.Windows.Forms.RichTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox_port
+            // 
+            this.textBox_port.Location = new System.Drawing.Point(88, 69);
+            this.textBox_port.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox_port.Name = "textBox_port";
+            this.textBox_port.Size = new System.Drawing.Size(138, 20);
+            this.textBox_port.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 70);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Port:";
+            // 
+            // button_listen
+            // 
+            this.button_listen.Location = new System.Drawing.Point(234, 66);
+            this.button_listen.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button_listen.Name = "button_listen";
+            this.button_listen.Size = new System.Drawing.Size(56, 22);
+            this.button_listen.TabIndex = 2;
+            this.button_listen.Text = "Listen";
+            this.button_listen.UseVisualStyleBackColor = true;
+            this.button_listen.Click += new System.EventHandler(this.button_listen_Click);
+            // 
+            // logs
+            // 
+            this.logs.Location = new System.Drawing.Point(42, 112);
+            this.logs.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.logs.Name = "logs";
+            this.logs.Size = new System.Drawing.Size(258, 171);
+            this.logs.TabIndex = 3;
+            this.logs.Text = "";
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(40, 301);
+            this.Label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(53, 13);
+            this.Label2.TabIndex = 4;
+            this.Label2.Text = "Message:";
+            // 
+            // textBox_message
+            // 
+            this.textBox_message.Enabled = false;
+            this.textBox_message.Location = new System.Drawing.Point(43, 328);
+            this.textBox_message.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox_message.Name = "textBox_message";
+            this.textBox_message.Size = new System.Drawing.Size(173, 20);
+            this.textBox_message.TabIndex = 5;
+            // 
+            // button_send
+            // 
+            this.button_send.Enabled = false;
+            this.button_send.Location = new System.Drawing.Point(234, 320);
+            this.button_send.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button_send.Name = "button_send";
+            this.button_send.Size = new System.Drawing.Size(59, 28);
+            this.button_send.TabIndex = 6;
+            this.button_send.Text = "send";
+            this.button_send.UseVisualStyleBackColor = true;
+            this.button_send.Click += new System.EventHandler(this.button_send_Click);
+            // 
+            // logs_users
+            // 
+            this.logs_users.Location = new System.Drawing.Point(333, 112);
+            this.logs_users.Margin = new System.Windows.Forms.Padding(2);
+            this.logs_users.Name = "logs_users";
+            this.logs_users.Size = new System.Drawing.Size(249, 171);
+            this.logs_users.TabIndex = 7;
+            this.logs_users.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(409, 72);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Connected Users";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(702, 72);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "IF100 Subscriptions";
+            // 
+            // logs_if100_subscriptions
+            // 
+            this.logs_if100_subscriptions.Location = new System.Drawing.Point(616, 112);
+            this.logs_if100_subscriptions.Margin = new System.Windows.Forms.Padding(2);
+            this.logs_if100_subscriptions.Name = "logs_if100_subscriptions";
+            this.logs_if100_subscriptions.Size = new System.Drawing.Size(259, 171);
+            this.logs_if100_subscriptions.TabIndex = 10;
+            this.logs_if100_subscriptions.Text = "";
+            // 
+            // logs_sps101_subscriptions
+            // 
+            this.logs_sps101_subscriptions.Location = new System.Drawing.Point(903, 112);
+            this.logs_sps101_subscriptions.Margin = new System.Windows.Forms.Padding(2);
+            this.logs_sps101_subscriptions.Name = "logs_sps101_subscriptions";
+            this.logs_sps101_subscriptions.Size = new System.Drawing.Size(259, 171);
+            this.logs_sps101_subscriptions.TabIndex = 11;
+            this.logs_sps101_subscriptions.Text = "";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(986, 72);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(112, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "SPS101 Subscriptions";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1199, 399);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.logs_sps101_subscriptions);
+            this.Controls.Add(this.logs_if100_subscriptions);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.logs_users);
+            this.Controls.Add(this.button_send);
+            this.Controls.Add(this.textBox_message);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.logs);
+            this.Controls.Add(this.button_listen);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_port);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_port;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_listen;
+        private System.Windows.Forms.RichTextBox logs;
+        private System.Windows.Forms.Label Label2;
+        private System.Windows.Forms.TextBox textBox_message;
+        private System.Windows.Forms.Button button_send;
+        private System.Windows.Forms.RichTextBox logs_users;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox logs_if100_subscriptions;
+        private System.Windows.Forms.RichTextBox logs_sps101_subscriptions;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
